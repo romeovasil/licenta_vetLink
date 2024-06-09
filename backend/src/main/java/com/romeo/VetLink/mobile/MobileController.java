cc06a4df57704ea0fa9855b4447db06046463cf3
romeovasil <romeo.vasil@irian.ro>
2024-06-09 11:11:28 +0300

    permit requests from mobile app to spring backend

--- backend/src/main/java/com/romeo/VetLink/mobile/MobileController.java
+++ backend/src/main/java/com/romeo/VetLink/mobile/MobileController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:58221", maxAge = 3600, allowCredentials="true")
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("api/v1/mobile")
 @RequiredArgsConstructor
